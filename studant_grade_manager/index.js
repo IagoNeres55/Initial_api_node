928a3fa8e8e5648f1b6af0b5589f7efececd0a21
IagoNeres55 <iagoneres90@gmail.com>
2024-09-03 17:42:41 -0300

    created crud

--- studant_grade_manager/index.js
+++ studant_grade_manager/index.js
@@ -1,8 +1,8 @@
 import http from "http";
-import {v4 as uuid} from "uuid";
+import { v4 as uuid } from "uuid";
 const port = 3000;
 
-const grades = [{ studentName: "John", subject: "English", grade: "8" }];
+const grades = [];
 
 const server = http.createServer((req, res) => {
   const { method, url } = req;
@@ -13,6 +13,8 @@ const server = http.createServer((req, res) => {
   });
 
   req.on("end", () => {
+    const id = url.split("/")[2];
+
     if (url === "/grades" && method === "GET") {
       res.writeHead(200, { "Content-Type": "application/json" });
       res.end(JSON.stringify(grades));
@@ -22,7 +24,30 @@ const server = http.createServer((req, res) => {
       grades.push(newGrade);
       res.writeHead(201, { "Content-Type": "application/json" });
       res.end(JSON.stringify(newGrade));
-      
+    } else if (url.startsWith("/grades/") && method === "PUT") {
+      const { studentName, subject, grade } = JSON.parse(body);
+      const gradeToUpdate = grades.find((g) => g.id === id);
+
+      if (gradeToUpdate) {
+        gradeToUpdate.studentName = studentName;
+        gradeToUpdate.subject = subject;
+        gradeToUpdate.grade = grade;
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(JSON.stringify(gradeToUpdate));
+      } else {
+        res.writeHead(404, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({ message: "Grade not found" }));
+      }
+    } else if (url.startsWith("/grades/") && method === "DELETE") {
+      const index = grades.findIndex((g) => g.id === id);
+      if (index !== -1) {
+        grades.splice(index, 1);
+        res.writeHead(204);
+        res.end(JSON.stringify({ message: "Grade deleted" }));
+      } else {
+        res.writeHead(404, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({ message: "Grade not found" }));
+      }
     } else {
       res.writeHead(404, { "Content-Type": "application/json" });
       res.end(JSON.stringify({ message: "Route not found" }));
